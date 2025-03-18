ccdc4e91172ed785ef0c9c803c801dc64e1e455a
piryth <arnaud.endignous@etu.univ-grenoble-alpes.fr>
2025-03-18 17:13:30 +0100

    fix(): backend

--- backend/hardhat.config.ts
+++ backend/hardhat.config.ts
@@ -1,45 +1,63 @@
-import * as dotenv from "dotenv";
-
 import { HardhatUserConfig, task } from "hardhat/config";
-import "@nomiclabs/hardhat-etherscan";
-import "@nomiclabs/hardhat-waffle";
-import "@typechain/hardhat";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomicfoundation/hardhat-ignition";
 import "hardhat-gas-reporter";
-import "solidity-coverage";
+import "solidity-coverage"
+ // /!\
+import "@nomicfoundation/hardhat-verify";
+ // /!\
 
-dotenv.config();
+ import 'dotenv/config'
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
+task("accounts", "Prints the list of accounts", async (args, hre) => {
   const accounts = await hre.ethers.getSigners();
-
   for (const account of accounts) {
     console.log(account.address);
   }
 });
 
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
+// Go to https://www.alchemy.com/, sign up, create a new API key
+// in its dashboard, and replace "KEY" with it
+const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL || "";
 
+// Replace this private key with your Sepolia account private key
+// To export your private key from Coinbase Wallet, go to
+// Settings > Developer Settings > Show private key
+// To export your private key from Metamask, open Metamask and
+// go to Account Details > Export Private Key
+// Beware: NEVER put real Ether into testing accounts
+const PRIVATE_KEY = process.env.PRIVATE_KEY || "";
+const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY || "";
+
+// Go to https://hardhat.org/config/ to learn more
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
 const config: HardhatUserConfig = {
-  solidity: "0.8.17",
+  defaultNetwork: "hardhat",
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    sepolia: {
+      url: SEPOLIA_RPC_URL,
+      accounts: PRIVATE_KEY.length > 0 ? [PRIVATE_KEY] : [],
+      chainId: 11155111,
     },
-    hardhat: {
-      chainId: 1337
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    enabled: true,
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: ETHERSCAN_API_KEY,
+  },
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.19",
+      },
+    ],
   },
 };
 
